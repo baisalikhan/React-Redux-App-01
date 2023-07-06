14787d0a08bd8cc1de1720d87f17bb4ee812158e
Bais Ali Khan <112929397+baisalikhan@users.noreply.github.com>
2023-07-06 12:46:48 +0500

    First App using vanilla Redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
+  const addBy = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  };
   return (
     <div className="App">
       <h1>This is my first redux application</h1>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
     </div>
   );
 }
